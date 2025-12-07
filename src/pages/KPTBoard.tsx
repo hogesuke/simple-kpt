2fd9c4ad94fdde2578259c2b7a15bd7cd9f1cf7b
hogesuke <miyado@gmail.com>
2025-12-07 20:00:47 +0900

    feat: CardInputコンポーネントをKPTBoardに組み込み

--- src/pages/KPTBoard.tsx
+++ src/pages/KPTBoard.tsx
@@ -1,13 +1,19 @@
 import { ReactElement } from 'react';
 
 import { BoardColumn } from '@/components/ui/BoardColumn';
+import { CardInput } from '@/components/ui/CardInput';
 
 export function KPTBoard(): ReactElement {
   return (
-    <section className="flex h-screen gap-x-4 p-8">
-      <BoardColumn title="Keep" type="keep" />
-      <BoardColumn title="Problem" type="problem" />
-      <BoardColumn title="Try" type="try" />
+    <section className="mx-auto grid h-screen w-full max-w-[1920px] grid-rows-[1fr_auto] gap-y-4 p-8">
+      <div className="flex flex-col items-stretch gap-x-4 gap-y-4 lg:flex-row">
+        <BoardColumn title="Keep" type="keep" />
+        <BoardColumn title="Problem" type="problem" />
+        <BoardColumn title="Try" type="try" />
+      </div>
+      <div>
+        <CardInput />
+      </div>
     </section>
   );
 }
